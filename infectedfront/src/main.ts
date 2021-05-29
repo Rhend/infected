b04ce01d67ac3b8cd0b76c3dc84305c376b47b6a
Rhend <rhend.project@gmail.com>
2021-05-30 01:08:46 +0200

    add v18n and begin prototyping

--- infectedfront/src/main.ts
+++ infectedfront/src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import messages from "./lang";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+
+const i18n = createI18n({
+    locale: 'fr',
+    fallbackLocale: 'fr',
+    messages
+  })
+createApp(App).use(store).use(router).use(i18n).mount("#app");
--- infectedfront/src/views/Home.vue
+++ infectedfront/src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    Bienvenue dans l'univers de Infected <br />
+    Nombres de personnes infectés : {{ infectedCount }}
   </div>
+
+  
+    <div v-if="infectedCount > 10 && infectedCount < 30">
+      Les gens commence à agir bizzarement..
+    </div>
+    <div v-else-if="infectedCount >= 30">
+      Voulez-vous envoyer vos infectés dans le quartier voisin ?
+      <button @click="spendInfected('Invade')">Envoyer les infectés</button>
+    </div>
+
+    <div v-if="invadeQuartier" class="block">Nombre d'infecté dans le quartier : {{ invadeQuartierCount }}</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      infectedCount: 0,
+      invadeQuartier: false,
+      invadeQuartierCount:0,
+    };
+  },
+  mounted() {
+    this.increaseInfected();
+  },
+  methods: {
+    increaseInfected() {
+      setInterval(() => {
+        this.infectedCount ++;
+      }, 1000);
+    },
+    spendInfected(whichStep: string){
+      console.info("Hello ", whichStep);
+      switch (whichStep) {
+        case "Invade":
+          this.infectedCount = this.infectedCount - 30;
+          this.invadeQuartier = true;
+          this.invadeQuartierCount += 30;
+          break;
+        default:
+          break;
+      }
+    },
   },
 });
 </script>
+<style lang="css">
+  .block{
+    border: 1px dotted black;
+    margin: 50px;
+  }
+</style>
