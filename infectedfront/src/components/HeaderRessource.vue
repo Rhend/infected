43afdfb0ec7e3c76fceeebce59b6a532330c026b
Rhend <rhend.project@gmail.com>
2021-06-06 18:51:23 +0200

    add first game loop with vuetify ! Hourra !

--- infectedfront/src/components/HeaderRessource.vue
+++ infectedfront/src/components/HeaderRessource.vue
@@ -1,5 +1,4 @@
-<template>
-  
+<template>  
     <v-app-bar app dark>
       <div class="d-flex align-center">
         <v-img
@@ -10,20 +9,20 @@
           transition="scale-transition"
           width="40"
         />
-
         Infected
       </div>
-
       <v-spacer></v-spacer>
+      Nombre d'infecté : {{ infectedCount }}
     </v-app-bar>
 </template>
 
 <script>
 export default {
   name: "HeaderRessource",
-
-  data: () => ({
-    //
-  }),
+    data() {
+        return {
+            infectedCount: 0,
+        }
+    },
 };
 </script>
